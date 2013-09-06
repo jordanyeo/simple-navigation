cc286e688ca33d1cd2e248eae219d80d19a31dcc
Jordan Yeo <jordan@yellowpencil.com>
2013-09-05 20:54:59 -0400

    custom

--- lib/simple_navigation/rendering/renderer/list.rb
+++ lib/simple_navigation/rendering/renderer/list.rb
@@ -10,7 +10,19 @@ module SimpleNavigation
     # The id can also be explicitely specified by setting the id in the html-options of the 'item' method in the config/navigation.rb file.
     class List < SimpleNavigation::Renderer::Base
       def render(item_container)
-        'jordantest'
+        list_content = item_container.items.inject([]) do |list, item|
+          li_options = item.html_options.reject {|k, v| k == :link}
+          li_content = tag_for(item)
+          if include_sub_navigation?(item)
+            li_content << render_sub_navigation_for(item)
+          end
+          list << content_tag(:li, li_content, li_options)
+        end.join
+        if skip_if_empty? && item_container.empty?
+          ''
+        else  
+          content_tag((options[:ordered] ? :ol : :ul), '<div class="menu_bg"></div><i class="icon-magic"></i><span>' + list_content + '</span>', {:id => item_container.dom_id, :class => item_container.dom_class}) 
+        end
       end
     end
   end
